9918468e364691b6504f34bc85736c2d914d8f5b
Zea <zeaear714@gmail.com>
2022-07-02 01:42:13 +0900

    Make basic Camera Movement

--- Assets/Character/RPG Tiny Hero Duo/Script/CameraMovement.cs
+++ Assets/Character/RPG Tiny Hero Duo/Script/CameraMovement.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
+
+    private Camera cam;
+    private Transform cameraArm;
+
     private void Start() {
-        
+        cam = GetComponentInChildren<Camera>();
+        cameraArm = GetComponent<Transform>();
     }
 
     private void Update() {
-        
+        CameraMove();
+    }
+
+    // 카메라 움직임을 담당하는 함수
+    private void CameraMove(){
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+
+        cameraArm.rotation = Quaternion.Euler(camAngle.x - mouseDelta.y, camAngle.y + mouseDelta.x, camAngle.z);
     }
 }
